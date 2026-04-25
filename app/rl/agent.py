7cfdabf440e23270832bb76ced53f598a7e21ed9
Nada <nadaghareeb1212@gmail.com>
2026-04-25 21:15:17 +0300

    feat: connect DQN network to RL agent

--- app/rl/agent.py
+++ app/rl/agent.py
@@ -1,23 +1,50 @@
 import random
+import torch
 
-# like gambling agent chooses randomly
-# Because in RL, agents usually start by exploring.
+from app.rl.network import DQNNetwork
+
+
 class DQNAgent:
-    def __init__(self, action_size=3):
+    def __init__(self, state_size=7, action_size=3):
         """
-        action_size: number of possible actions.
-        In our case:
-        0 = hold
-        1 = buy
-        2 = sell
+        state_size: number of inputs (prices + balance + position)
+        action_size: number of actions (hold, buy, sell)
         """
+        self.state_size = state_size
         self.action_size = action_size
+
+        # neural network (brain)
+        self.network = DQNNetwork(state_size, action_size)
+
+        # exploration rate
+        self.epsilon = 1.0  # start fully random
+
+    def preprocess_state(self, state):
+        """
+        Convert state dict → flat tensor
+        """
+        prices = state["prices"]
+        balance = state["balance"]
+        position = state["position"]
+
+        state_list = prices + [balance, position]
+
+        return torch.tensor([state_list], dtype=torch.float32)
 
     def choose_action(self, state):
         """
-        Choose an action for the current state.
-
-        For now, the agent acts randomly.
-        Later, i will replace this with a neural network.
+        Epsilon-greedy action selection
         """
-        return random.randint(0, self.action_size - 1)
+        # random action
+        if random.random() < self.epsilon:
+            return random.randint(0, self.action_size - 1)
+
+        # use neural network
+        state_tensor = self.preprocess_state(state)
+
+        with torch.no_grad():
+            q_values = self.network(state_tensor)
+
+        action = torch.argmax(q_values).item()
+
+        return action
